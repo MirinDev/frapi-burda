d0f757083d316028255280f20c00e16d05b67e78
MirinDev <92026590+MirinDev@users.noreply.github.com>
2022-12-08 20:16:32 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
-gcc -o build/terminal resources/resources.res Main.c src/terminal.c -Iinclude -Wall -static -static-libgcc 
-C:\"Program Files (x86)"\"Microsoft SDKs"\ClickOnce\SignTool\signtool sign /fd SHA256 /p "JamDev_Licence1" /f "build/legal.pfx" build/terminal.exe
+gcc -o build/terminal resources/resources.res Main.c src/terminal.c -Iinclude -Wall -static -static-libgcc
 start .\build\terminal.exe
